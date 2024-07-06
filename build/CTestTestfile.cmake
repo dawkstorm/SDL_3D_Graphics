48eae0d4b6d684f761c6342d951519d593de79c8
Dawkstorm <john.kalmykov@gmail.com>
2024-07-06 22:34:02 +0300

    remove cmake from github

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dawkstorm/VSCode_Projects/SDL_3D_Graphics
-# Build directory: /home/dawkstorm/VSCode_Projects/SDL_3D_Graphics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
